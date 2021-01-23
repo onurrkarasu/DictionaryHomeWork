de73a028aef4973f9988690684d5c2a5c70fb2fc
Orjinal parfumcum <orjinalparfumcum@outlook.com>
2021-01-23 04:18:15 +0300

    Add project files.

---
+++ DictionaryHomeWork/Dictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryHomeWork
+{
+    class MyDictionary<T, V>
+    {
+        T[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new T[0];
+            values = new V[0];
+        }
+
+        public void Add(T key, V value)
+        {
+            T[] tempArrayKeys = keys;
+            V[] tempArrayValues = values;
+            keys = new T[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempArrayKeys.Length; i++)
+            {
+                keys[i] = tempArrayKeys[i];
+                values[i] = tempArrayValues[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public int Count
+        {
+            get { return values.Length; }
+
+        }
+        public V[] Values
+        {
+            get { return values; }
+        }
+
+
+
+    }
+}
---
+++ DictionaryHomeWork/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DictionaryHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> Customer = new MyDictionary<int, string>();
+            Customer.Add(101, "Onur");
+            Customer.Add(202, "Hasan");
+            Customer.Add(303, "Metin");
+
+            Console.WriteLine(Customer.Count);
+            Console.WriteLine("--------------");
+            foreach (var name in Customer.Values)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
